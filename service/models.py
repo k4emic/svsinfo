8f8461d4322fd414fc877c430e40f4a449f8e26b
Mads Nielsen <k4emic@gmail.com>
2012-10-16 14:49:20 +0200

    Added __unicode__ method to models

--- service/models.py
+++ service/models.py
@@ -3,15 +3,24 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=50)
     
+    def __unicode__(self):
+        return self.name
+    
 class Area(models.Model):
     name = models.CharField(max_length=50)
     location = models.ForeignKey(Location)
+    
+    def __unicode__(self):
+        return self.name
 
 class Event(models.Model):
     name = models.CharField(max_length=50)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     location = models.ForeignKey(Location)
+    
+    def __unicode__(self):
+        return self.name
 
 class Activity(models.Model):
     name = models.CharField(max_length=100)
@@ -19,6 +28,9 @@ class Activity(models.Model):
     end_time = models.DateTimeField()
     event = models.ForeignKey(Event)
     area = models.ForeignKey(Area)
+    
+    def __unicode__(self):
+        return self.name
 
 class NewsItem(models.Model):
     title = models.CharField(max_length=100)
@@ -26,3 +38,6 @@ class NewsItem(models.Model):
     time = models.DateTimeField()
     event = models.ForeignKey(Event, null=True)
     activity = models.ForeignKey(Activity, null=True)
+    
+    def __unicode__(self):
+        return self.title
