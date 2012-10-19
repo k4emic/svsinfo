2359f30b1183ec66dbbdcc71b5469ef9392ec833
Mads Nielsen <k4emic@gmail.com>
2012-10-19 21:05:26 +0200

    Basic usage of restframework implemented

--- service/urls.py
+++ service/urls.py
@@ -1,5 +1,25 @@
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
+from service import models
 
-urlpatterns = patterns('service.views',
+# rest framework
+from djangorestframework.resources import ModelResource
+from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+
+class LocationResource(ModelResource):
+    model = models.Location
     
-); 
+class AreaResource(ModelResource):
+    model = models.Area
+    
+def get_standard_patterns(url_prefix, model_resource):
+
+    base_url = r"^{prefix}/$".format(prefix=url_prefix)
+    pk_url = r"^{prefix}/(?P<pk>[^/]+)/$".format(prefix=url_prefix)
+    
+    return patterns('',
+        url(base_url, ListOrCreateModelView.as_view(resource=model_resource)),
+        url(pk_url, InstanceModelView.as_view(resource=model_resource))
+    )
+
+urlpatterns = get_standard_patterns('locations', LocationResource)
+urlpatterns += get_standard_patterns('areas', AreaResource)
--- svsinfo2/urls.py
+++ svsinfo2/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^api/', include('service.urls')),
-    
-    
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('djangorestframework.urls', namespace='djangorestframework'))
 )
