800c7d0adf18d913a5d6a82058cff6af6cccbae2
Mads Nielsen <k4emic@gmail.com>
2012-10-21 18:33:07 +0200

    Working read-only REST service

--- service/model_resources.py
+++ service/model_resources.py
@@ -1,23 +1,66 @@
 from djangorestframework.resources import ModelResource
+from djangorestframework.reverse import reverse
 from service import models
 
 class LocationResource(ModelResource):
     model = models.Location
-    exclude = None
+    fields = ['id', 'name', 'areas']
+    
+    def areas(self, instance):
+        return reverse('areas',
+                       kwargs={'location': instance.id}, 
+                       request=self.request)
     
 class AreaResource(ModelResource):
     model = models.Area
-    exclude = None
+    fields = ['id', 'name', 'location']
+    
+    def location(self, instance):
+        return reverse('location', 
+                       kwargs={'pk': instance.location_id}, 
+                       request=self.request)
     
 class EventResource(ModelResource):
     model = models.Event
-    exclude = None
+    fields = ['id', 'name', 'start_time', 'end_time', 'location', 'activities']
+    
+    def location(self, instance):
+        return reverse('location', 
+                       kwargs={'pk': instance.location_id},
+                       request = self.request)
+        
+    def activities(self, instance):
+        return reverse('activities', 
+                       kwargs={'event': instance.id},
+                       request=self.request)
     
 class ActivityResource(ModelResource):
     model = models.Activity
-    exclude = None
+    fields = ['id', 'name', 'start_time', 'end_time', 'area', 'news']
+    
+    def area(self, instance):
+        return reverse('area', 
+                       kwargs={'pk': instance.area_id},
+                       request=self.request)
+   
+    def news(self, instance):
+        return reverse('activity-news', 
+                       kwargs={'activity': instance.id},
+                       request=self.request)
     
 class NewsItemResource(ModelResource):
     model = models.NewsItem
-    exclude = None
+    fields = ['id', 'title', 'text', 'event', 'activity']
     
+    def event(self, instance):
+        return reverse('event',
+                       kwargs={'pk': instance.event_id},
+                       request=self.request)
+    
+    def activity(self, instance):
+        if(instance.activity):
+            return reverse('activity',
+                       kwargs={'pk': instance.event_id},
+                       request=self.request)
+        else:
+            return ""
--- service/urls.py
+++ service/urls.py
@@ -8,22 +8,23 @@ urlpatterns = patterns('',
     # /events 
     url(r'^events/$', ListOrCreateModelView.as_view(resource=EventResource)),
     # /events/<id>
-    url(r'^events/(?P<pk>[\d]+)/$', InstanceModelView.as_view(resource=EventResource)),
+    url(r'^events/(?P<pk>[\d]+)/$', InstanceModelView.as_view(resource=EventResource), name="event"),
     
     # /events/<event>/activities
-    url(r'^events/(?P<event>[\d]+)/activities/$', ListOrCreateModelView.as_view(resource=ActivityResource)),
-    # /events/<event>/activities/<id>
-    url(r'^events/(?P<event>[\d]+)/activities/(?P<pk>[\d]+)/$', InstanceModelView.as_view(resource=ActivityResource)),
+    url(r'^events/(?P<event>[\d]+)/activities/$', ListOrCreateModelView.as_view(resource=ActivityResource), name="activities"),
+    # /activities/<id>
+    url(r'^activities/(?P<pk>[\d]+)/$', InstanceModelView.as_view(resource=ActivityResource), name="activity"),
     
-    # /locations
-    url(r'^locations/$', ListOrCreateModelView.as_view(resource=LocationResource)),
-    # /locations/<id>/areas
-    url(r'^locations/(?P<location>[\d]+)/areas/$', ListOrCreateModelView.as_view(resource=AreaResource)),
+    # locations/<id>
+    url(r'^locations/(?P<pk>[\d]+)/$', ListOrCreateModelView.as_view(resource=LocationResource), name="location"),
+    # locations/<id>/areas
+    url(r'^locations/(?P<location>[\d]+)/areas/$', ListOrCreateModelView.as_view(resource=AreaResource), name="areas"),
+    # areas/<id>
+    url(r'^areas/(?P<pk>[\d]+)/$', InstanceModelView.as_view(resource=AreaResource), name="area"),
     
+    # /activity/<id>/news
+    url(r'^activity/(?P<activity>[\d]+)/news/$', ListOrCreateModelView.as_view(resource=NewsItemResource), name='activity-news'),
     # /events/<event-id>/news
-    url(r'^events/(?P<event>[\d]+)/news/$', ListOrCreateModelView.as_view(resource=NewsItemResource)),
-    
-    # /events/<event-id>/news/<id>
-    url(r'^events/(?P<event>[\d]+)/news/(?P<pk>[\d+])/$', InstanceModelView.as_view(resource=NewsItemResource)),
+    url(r'^events/(?P<event>[\d]+)/news/$', ListOrCreateModelView.as_view(resource=NewsItemResource), name="event-news"),
     
 )
