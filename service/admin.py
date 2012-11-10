13c0690be508f7414c1a4752bafe668ce25a2023
Mads Nielsen <k4emic@gmail.com>
2012-11-10 16:55:31 +0100

    Changed admin interface

--- service/admin.py
+++ service/admin.py
@@ -1,8 +1,12 @@
 from service.models import Location, Area, Event, Activity, NewsItem
 from django.contrib import admin
 
+class ActivityAdmin(admin.ModelAdmin):
+    ordering = ('start_time', )
+    list_display = ('name', 'start_time', 'end_time', 'area')
+
 admin.site.register(Location)
 admin.site.register(Area)
 admin.site.register(Event)
-admin.site.register(Activity)
+admin.site.register(Activity, ActivityAdmin)
 admin.site.register(NewsItem)
