7c1a782f22e6d18dc1cf68a9f772c8449bf776d8
Mads Nielsen <k4emic@gmail.com>
2013-01-27 17:56:32 +0100

    Data for template basic template removed unused bootstrap css files

--- svsinfo2/urls.py
+++ svsinfo2/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^client/', include('webclient.urls')),
+    url(r'^', include('webclient.urls')),
     url(r'^api/', include('service.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- webclient/views.py
+++ webclient/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from service import models
+import django.utils.timezone
 
 # Create your views here.
 def index(request):
-    context = {'message': 'Hello world!'}
-    return render_to_response('hello.html', context, RequestContext(request))
+    # get current con
+    con = models.Convention.objects.latest('start_time')
+    events = con.event_set.order_by('start_time').all()
     
+    current_events = []
+    future_events = []
+    past_events = []
+    
+    now = django.utils.timezone.now()
+    for event in events:
+        if(event.start_time < now and now < event.end_time): # current event
+            current_events.append(event)
+        elif(event.end_time > now): # future event
+            future_events.append(event)
+        else:
+            # past events are not interesting
+            past_events.append(event)
+    
+    context = {
+        'convention': con,
+        'current_events': current_events,
+        'future_events': future_events,
+        'past_events': past_events
+        
+    }
+    return render_to_response('index.html', context, RequestContext(request))
