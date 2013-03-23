3b36a18b8f271535eda4265fbace0199607e45b3
Mads Nielsen <k4emic@gmail.com>
2013-03-23 16:50:05 +0100

    Update for event slider. Removed bootstrap css file

--- webclient/static/js/events.js
+++ webclient/static/js/events.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function($) {
     $('.event-item').click(function() {
-        $('.event-description').slideUp();
+        
         $desc = $(this).children('.event-description').first();
+        
+        $('.event-description').not($desc).slideUp();
+        
         if($desc.text().length > 0) {
             $desc.slideDown();
         }
